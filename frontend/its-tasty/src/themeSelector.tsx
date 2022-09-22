b2b56185a2a1363410ff75baf5e7b0ccf18a6952
Luca Wilke <luw8000@gmail.com>
2022-09-22 22:35:26 +0200

    added darkmode
    close #50

---
+++ frontend/its-tasty/src/themeSelector.tsx
@@ -0,0 +1,25 @@
+import * as React from 'react';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
+export function ThemeSelector(props : any) {
+    const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+
+    const theme = React.useMemo(
+        () =>
+            createTheme({
+                palette: {
+                    mode: prefersDarkMode ? 'dark' : 'light',
+                },
+            }),
+        [prefersDarkMode],
+    );
+
+    return (
+        <ThemeProvider theme={theme}>
+            <CssBaseline />
+            {props.children}
+        </ThemeProvider>
+    );
+}
