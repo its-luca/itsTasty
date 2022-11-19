037beeb5f62f7ff8edcafac1afb990d856aa0a50
Luca Wilke <luw8000@gmail.com>
2022-11-19 20:42:53 +0100

    removed sql boiler tests as they cannot be skipped and thus make the testpipeline fail
    when re-generating the sqlboiler code we do two passes.
    In the first pass we generate and run the tets and in the second pass we delete them

--- pkg/api/ports/botAPI/dish-server.gen.go
+++ pkg/api/ports/botAPI/dish-server.gen.go
@@ -10,6 +10,7 @@ import (
 	"encoding/base64"
 	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 	"net/url"
 	"path"
@@ -19,6 +20,396 @@ import (
 	"github.com/getkin/kin-openapi/openapi3"
 	"github.com/go-chi/chi/v5"
 )
+
+// RequestEditorFn  is the function signature for the RequestEditor callback function
+type RequestEditorFn func(ctx context.Context, req *http.Request) error
+
+// Doer performs HTTP requests.
+//
+// The standard http.Client implements this interface.
+type HttpRequestDoer interface {
+	Do(req *http.Request) (*http.Response, error)
+}
+
+// Client which conforms to the OpenAPI3 specification for this service.
+type Client struct {
+	// The endpoint of the server conforming to this interface, with scheme,
+	// https://api.deepmap.com for example. This can contain a path relative
+	// to the server, such as https://api.deepmap.com/dev-test, and all the
+	// paths in the swagger spec will be appended to the server.
+	Server string
+
+	// Doer for performing requests, typically a *http.Client with any
+	// customized settings, such as certificate chains.
+	Client HttpRequestDoer
+
+	// A list of callbacks for modifying requests which are generated before sending over
+	// the network.
+	RequestEditors []RequestEditorFn
+}
+
+// ClientOption allows setting custom parameters during construction
+type ClientOption func(*Client) error
+
+// Creates a new Client, with reasonable defaults
+func NewClient(server string, opts ...ClientOption) (*Client, error) {
+	// create a client with sane default values
+	client := Client{
+		Server: server,
+	}
+	// mutate client and add all optional params
+	for _, o := range opts {
+		if err := o(&client); err != nil {
+			return nil, err
+		}
+	}
+	// ensure the server URL always has a trailing slash
+	if !strings.HasSuffix(client.Server, "/") {
+		client.Server += "/"
+	}
+	// create httpClient, if not already present
+	if client.Client == nil {
+		client.Client = &http.Client{}
+	}
+	return &client, nil
+}
+
+// WithHTTPClient allows overriding the default Doer, which is
+// automatically created using http.Client. This is useful for tests.
+func WithHTTPClient(doer HttpRequestDoer) ClientOption {
+	return func(c *Client) error {
+		c.Client = doer
+		return nil
+	}
+}
+
+// WithRequestEditorFn allows setting up a callback function, which will be
+// called right before sending the request. This can be used to mutate the request.
+func WithRequestEditorFn(fn RequestEditorFn) ClientOption {
+	return func(c *Client) error {
+		c.RequestEditors = append(c.RequestEditors, fn)
+		return nil
+	}
+}
+
+// The interface specification for the client above.
+type ClientInterface interface {
+	// PostCreateOrUpdateDish request with any body
+	PostCreateOrUpdateDishWithBody(ctx context.Context, contentType string, body io.Reader, reqEditors ...RequestEditorFn) (*http.Response, error)
+
+	PostCreateOrUpdateDish(ctx context.Context, body PostCreateOrUpdateDishJSONRequestBody, reqEditors ...RequestEditorFn) (*http.Response, error)
+
+	// GetDishesDishID request
+	GetDishesDishID(ctx context.Context, dishID int64, reqEditors ...RequestEditorFn) (*http.Response, error)
+}
+
+func (c *Client) PostCreateOrUpdateDishWithBody(ctx context.Context, contentType string, body io.Reader, reqEditors ...RequestEditorFn) (*http.Response, error) {
+	req, err := NewPostCreateOrUpdateDishRequestWithBody(c.Server, contentType, body)
+	if err != nil {
+		return nil, err
+	}
+	req = req.WithContext(ctx)
+	if err := c.applyEditors(ctx, req, reqEditors); err != nil {
+		return nil, err
+	}
+	return c.Client.Do(req)
+}
+
+func (c *Client) PostCreateOrUpdateDish(ctx context.Context, body PostCreateOrUpdateDishJSONRequestBody, reqEditors ...RequestEditorFn) (*http.Response, error) {
+	req, err := NewPostCreateOrUpdateDishRequest(c.Server, body)
+	if err != nil {
+		return nil, err
+	}
+	req = req.WithContext(ctx)
+	if err := c.applyEditors(ctx, req, reqEditors); err != nil {
+		return nil, err
+	}
+	return c.Client.Do(req)
+}
+
+func (c *Client) GetDishesDishID(ctx context.Context, dishID int64, reqEditors ...RequestEditorFn) (*http.Response, error) {
+	req, err := NewGetDishesDishIDRequest(c.Server, dishID)
+	if err != nil {
+		return nil, err
+	}
+	req = req.WithContext(ctx)
+	if err := c.applyEditors(ctx, req, reqEditors); err != nil {
+		return nil, err
+	}
+	return c.Client.Do(req)
+}
+
+// NewPostCreateOrUpdateDishRequest calls the generic PostCreateOrUpdateDish builder with application/json body
+func NewPostCreateOrUpdateDishRequest(server string, body PostCreateOrUpdateDishJSONRequestBody) (*http.Request, error) {
+	var bodyReader io.Reader
+	buf, err := json.Marshal(body)
+	if err != nil {
+		return nil, err
+	}
+	bodyReader = bytes.NewReader(buf)
+	return NewPostCreateOrUpdateDishRequestWithBody(server, "application/json", bodyReader)
+}
+
+// NewPostCreateOrUpdateDishRequestWithBody generates requests for PostCreateOrUpdateDish with any type of body
+func NewPostCreateOrUpdateDishRequestWithBody(server string, contentType string, body io.Reader) (*http.Request, error) {
+	var err error
+
+	serverURL, err := url.Parse(server)
+	if err != nil {
+		return nil, err
+	}
+
+	operationPath := fmt.Sprintf("/createOrUpdateDish")
+	if operationPath[0] == '/' {
+		operationPath = "." + operationPath
+	}
+
+	queryURL, err := serverURL.Parse(operationPath)
+	if err != nil {
+		return nil, err
+	}
+
+	req, err := http.NewRequest("POST", queryURL.String(), body)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Add("Content-Type", contentType)
+
+	return req, nil
+}
+
+// NewGetDishesDishIDRequest generates requests for GetDishesDishID
+func NewGetDishesDishIDRequest(server string, dishID int64) (*http.Request, error) {
+	var err error
+
+	var pathParam0 string
+
+	pathParam0, err = runtime.StyleParamWithLocation("simple", false, "dishID", runtime.ParamLocationPath, dishID)
+	if err != nil {
+		return nil, err
+	}
+
+	serverURL, err := url.Parse(server)
+	if err != nil {
+		return nil, err
+	}
+
+	operationPath := fmt.Sprintf("/dishes/%s", pathParam0)
+	if operationPath[0] == '/' {
+		operationPath = "." + operationPath
+	}
+
+	queryURL, err := serverURL.Parse(operationPath)
+	if err != nil {
+		return nil, err
+	}
+
+	req, err := http.NewRequest("GET", queryURL.String(), nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return req, nil
+}
+
+func (c *Client) applyEditors(ctx context.Context, req *http.Request, additionalEditors []RequestEditorFn) error {
+	for _, r := range c.RequestEditors {
+		if err := r(ctx, req); err != nil {
+			return err
+		}
+	}
+	for _, r := range additionalEditors {
+		if err := r(ctx, req); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// ClientWithResponses builds on ClientInterface to offer response payloads
+type ClientWithResponses struct {
+	ClientInterface
+}
+
+// NewClientWithResponses creates a new ClientWithResponses, which wraps
+// Client with return type handling
+func NewClientWithResponses(server string, opts ...ClientOption) (*ClientWithResponses, error) {
+	client, err := NewClient(server, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return &ClientWithResponses{client}, nil
+}
+
+// WithBaseURL overrides the baseURL.
+func WithBaseURL(baseURL string) ClientOption {
+	return func(c *Client) error {
+		newBaseURL, err := url.Parse(baseURL)
+		if err != nil {
+			return err
+		}
+		c.Server = newBaseURL.String()
+		return nil
+	}
+}
+
+// ClientWithResponsesInterface is the interface specification for the client with responses above.
+type ClientWithResponsesInterface interface {
+	// PostCreateOrUpdateDish request with any body
+	PostCreateOrUpdateDishWithBodyWithResponse(ctx context.Context, contentType string, body io.Reader, reqEditors ...RequestEditorFn) (*PostCreateOrUpdateDishResponse, error)
+
+	PostCreateOrUpdateDishWithResponse(ctx context.Context, body PostCreateOrUpdateDishJSONRequestBody, reqEditors ...RequestEditorFn) (*PostCreateOrUpdateDishResponse, error)
+
+	// GetDishesDishID request
+	GetDishesDishIDWithResponse(ctx context.Context, dishID int64, reqEditors ...RequestEditorFn) (*GetDishesDishIDResponse, error)
+}
+
+type PostCreateOrUpdateDishResponse struct {
+	Body         []byte
+	HTTPResponse *http.Response
+	JSON200      *CreateOrUpdateDishResp
+	JSON500      *BasicError
+}
+
+// Status returns HTTPResponse.Status
+func (r PostCreateOrUpdateDishResponse) Status() string {
+	if r.HTTPResponse != nil {
+		return r.HTTPResponse.Status
+	}
+	return http.StatusText(0)
+}
+
+// StatusCode returns HTTPResponse.StatusCode
+func (r PostCreateOrUpdateDishResponse) StatusCode() int {
+	if r.HTTPResponse != nil {
+		return r.HTTPResponse.StatusCode
+	}
+	return 0
+}
+
+type GetDishesDishIDResponse struct {
+	Body         []byte
+	HTTPResponse *http.Response
+	JSON200      *GetDishResp
+	JSON400      *BasicError
+	JSON500      *BasicError
+}
+
+// Status returns HTTPResponse.Status
+func (r GetDishesDishIDResponse) Status() string {
+	if r.HTTPResponse != nil {
+		return r.HTTPResponse.Status
+	}
+	return http.StatusText(0)
+}
+
+// StatusCode returns HTTPResponse.StatusCode
+func (r GetDishesDishIDResponse) StatusCode() int {
+	if r.HTTPResponse != nil {
+		return r.HTTPResponse.StatusCode
+	}
+	return 0
+}
+
+// PostCreateOrUpdateDishWithBodyWithResponse request with arbitrary body returning *PostCreateOrUpdateDishResponse
+func (c *ClientWithResponses) PostCreateOrUpdateDishWithBodyWithResponse(ctx context.Context, contentType string, body io.Reader, reqEditors ...RequestEditorFn) (*PostCreateOrUpdateDishResponse, error) {
+	rsp, err := c.PostCreateOrUpdateDishWithBody(ctx, contentType, body, reqEditors...)
+	if err != nil {
+		return nil, err
+	}
+	return ParsePostCreateOrUpdateDishResponse(rsp)
+}
+
+func (c *ClientWithResponses) PostCreateOrUpdateDishWithResponse(ctx context.Context, body PostCreateOrUpdateDishJSONRequestBody, reqEditors ...RequestEditorFn) (*PostCreateOrUpdateDishResponse, error) {
+	rsp, err := c.PostCreateOrUpdateDish(ctx, body, reqEditors...)
+	if err != nil {
+		return nil, err
+	}
+	return ParsePostCreateOrUpdateDishResponse(rsp)
+}
+
+// GetDishesDishIDWithResponse request returning *GetDishesDishIDResponse
+func (c *ClientWithResponses) GetDishesDishIDWithResponse(ctx context.Context, dishID int64, reqEditors ...RequestEditorFn) (*GetDishesDishIDResponse, error) {
+	rsp, err := c.GetDishesDishID(ctx, dishID, reqEditors...)
+	if err != nil {
+		return nil, err
+	}
+	return ParseGetDishesDishIDResponse(rsp)
+}
+
+// ParsePostCreateOrUpdateDishResponse parses an HTTP response from a PostCreateOrUpdateDishWithResponse call
+func ParsePostCreateOrUpdateDishResponse(rsp *http.Response) (*PostCreateOrUpdateDishResponse, error) {
+	bodyBytes, err := io.ReadAll(rsp.Body)
+	defer func() { _ = rsp.Body.Close() }()
+	if err != nil {
+		return nil, err
+	}
+
+	response := &PostCreateOrUpdateDishResponse{
+		Body:         bodyBytes,
+		HTTPResponse: rsp,
+	}
+
+	switch {
+	case strings.Contains(rsp.Header.Get("Content-Type"), "json") && rsp.StatusCode == 200:
+		var dest CreateOrUpdateDishResp
+		if err := json.Unmarshal(bodyBytes, &dest); err != nil {
+			return nil, err
+		}
+		response.JSON200 = &dest
+
+	case strings.Contains(rsp.Header.Get("Content-Type"), "json") && rsp.StatusCode == 500:
+		var dest BasicError
+		if err := json.Unmarshal(bodyBytes, &dest); err != nil {
+			return nil, err
+		}
+		response.JSON500 = &dest
+
+	}
+
+	return response, nil
+}
+
+// ParseGetDishesDishIDResponse parses an HTTP response from a GetDishesDishIDWithResponse call
+func ParseGetDishesDishIDResponse(rsp *http.Response) (*GetDishesDishIDResponse, error) {
+	bodyBytes, err := io.ReadAll(rsp.Body)
+	defer func() { _ = rsp.Body.Close() }()
+	if err != nil {
+		return nil, err
+	}
+
+	response := &GetDishesDishIDResponse{
+		Body:         bodyBytes,
+		HTTPResponse: rsp,
+	}
+
+	switch {
+	case strings.Contains(rsp.Header.Get("Content-Type"), "json") && rsp.StatusCode == 200:
+		var dest GetDishResp
+		if err := json.Unmarshal(bodyBytes, &dest); err != nil {
+			return nil, err
+		}
+		response.JSON200 = &dest
+
+	case strings.Contains(rsp.Header.Get("Content-Type"), "json") && rsp.StatusCode == 400:
+		var dest BasicError
+		if err := json.Unmarshal(bodyBytes, &dest); err != nil {
+			return nil, err
+		}
+		response.JSON400 = &dest
+
+	case strings.Contains(rsp.Header.Get("Content-Type"), "json") && rsp.StatusCode == 500:
+		var dest BasicError
+		if err := json.Unmarshal(bodyBytes, &dest); err != nil {
+			return nil, err
+		}
+		response.JSON500 = &dest
+
+	}
+
+	return response, nil
+}
 
 // ServerInterface represents all server handlers.
 type ServerInterface interface {
