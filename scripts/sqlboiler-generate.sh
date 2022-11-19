425092c0e1fdd0f9b589374ad444bd2176f53b6d
Luca Wilke <luw8000@gmail.com>
2022-11-19 20:42:53 +0100

    removed sql boiler tests as they cannot be skipped and thus make the testpipeline fail
    when re-generating the sqlboiler code we do two passes.
    In the first pass we generate and run the tets and in the second pass we delete them

--- scripts/sqlboiler-generate.sh
+++ scripts/sqlboiler-generate.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 sqlboiler  --wipe -p sqlboilerPSQL -o pkg/api/adapters/dishRepo/sqlboilerPSQL psql || exit 1
-go test ./pkg/api/adapters/dishRepo/sqlboilerPSQL
+go test ./pkg/api/adapters/dishRepo/sqlboilerPSQL || exit 1
+sqlboiler  --wipe --no-tests -p sqlboilerPSQL -o pkg/api/adapters/dishRepo/sqlboilerPSQL psql || exit 1
