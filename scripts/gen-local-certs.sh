d883e9a31b3df2753e321ebbdcd3b1738297f5d7
Luca Wilke <luw8000@gmail.com>
2023-04-22 11:17:55 +0200

    fixed path in local certs scripts

--- scripts/gen-local-certs.sh
+++ scripts/gen-local-certs.sh
@@ -2,4 +2,4 @@
 mkdir -p testdata/selfSignedTLS
 
 openssl ecparam -genkey -name secp384r1 -out testdata/selfSignedTLS/server.key
-openssl req -new -x509 -sha256 -key server.key -out testdata/selfSignedTLS/server.crt -days 3650
+openssl req -new -x509 -sha256 -key ./testdata/selfSignedTLS/server.key -out testdata/selfSignedTLS/server.crt -days 3650
