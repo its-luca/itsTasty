e79762ba176e16105dbd84406ba2dc36510060b3
Luca Wilke <luw8000@gmail.com>
2023-04-24 21:58:13 +0200

    allow name change in patch merged dish endpoint

--- frontend/its-tasty/src/services/userAPI/models/MergedDishUpdateReq.ts
+++ frontend/its-tasty/src/services/userAPI/models/MergedDishUpdateReq.ts
@@ -14,5 +14,9 @@ export type MergedDishUpdateReq = {
      * If present, these IDs are removed from the merged dish. At least two dish must remain. \ To delete a merge dish, use DELETE instead of PATCH
      */
     removeDishIDs?: Array<number>;
+    /**
+     * If present, the merged dish will be renamed to this
+     */
+    name?: string;
 };
 
